4ba10bc84b31907b26e2c7c1f886b142c9077608
Gameel-Allam <fadyallam8@gmail.com>
2023-02-09 23:56:02 +0200

    Finsih forecast logic and styling

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,42 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./Forecast.module.css";
 
 const Forecast = ({ forecastData }) => {
 
     const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const day = new Date().getDay();
-    const nextWeekDays = days.slice(day, days.length).concat(days.slice(0, day));
+    const nextWeekDays = days.slice(new Date().getDay(), days.length).concat(days.slice(0, new Date().getDay()));
     const nextWeekForecast = forecastData.list.slice(0, 7);
-    console.log(nextWeekForecast);
+
+    const [description, setDescription] = useState(forecastData.list[0].main);
+    const [activeId, setActiveId] = useState(0);
+
+    const handleSelect = (idx) => {
+        setDescription(nextWeekForecast[idx].main);
+        setActiveId(idx);
+    }
+
+    console.log("mounted");
+
+    useEffect(() => {
+        setDescription(forecastData.list[0].main)
+    }, [forecastData])
 
     return (
         <div className={styles.card}>
             <div className={styles.description}>
                 <div className={styles.item}>
-                    <label>PRECIPITATION</label>
-                    <p>0 %</p>
+                    <label>Feels like</label>
+                    <p>{description.feels_like}°C</p>
                 </div>
                 <div className={styles.item}>
-                    <label>HUMIDITY</label>
-                    <p>34 %</p>
+                    <label>Humidity</label>
+                    <p>{description.humidity} %</p>
                 </div>
                 <div className={styles.item}>
-                    <label>WIND</label>
-                    <p>0 km/h</p>
+                    <label>Pressure</label>
+                    <p>{description.pressure} Pa</p>
                 </div>
             </div>
             <div className={styles.week}>
                 {nextWeekForecast.map((ele, idx) => {
                     return (
-                        <div className={styles.day} key={idx}>
+                        <div className={`${styles.day} ${idx === activeId ? styles.active : ""}`} key={idx} onClick={() => handleSelect(idx)}>
                             <img src={`assets/${ele.weather[0].icon}.png`} alt='icon' />
                             <label>{nextWeekDays[idx]}</label>
                             <p>{ele.main.temp}°C</p>
                         </div>)
                 })}
             </div>
-            <button>Change the location</button>
-        </div>
+            <p className={styles.quote}>Have a nice day ❤️</p>
+        </div >
     )
 }
 
